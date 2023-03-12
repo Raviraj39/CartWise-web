39b4d026e549597896b63856349468d4c48b55ee
Raviraj39 <rravirajbujawade@gmail.com>
2023-03-12 23:34:40 +0530

    Added Checkout Page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Switch, Route } from "react-router-dom";
 import Product from "./component/Product";
 import ProductView from "./component/ProductView";
 import Cart from "./component/Cart";
+import Checkout from "./component/Checkout";
 function App() {
   return (
     <>
@@ -14,6 +15,7 @@ function App() {
         <Route exact path="/products" component={Product} />
         <Route exact path="/products/:id" component={ProductView} />
         <Route exact path="/cart" component={Cart}/>
+        <Route exact path="/checkout" component={Checkout}/>
       </Switch>
     </>
   );
